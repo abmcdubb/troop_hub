1893c73283e92a8296be3bd9841b7bdcd9e13061
emmaife <emma.s.ife@gmail.com>
2014-04-08 11:13:59 -0400

    generated troop model

---
+++ db/migrate/20140408150522_create_troops.rb
@@ -0,0 +1,16 @@
+class CreateTroops < ActiveRecord::Migration
+  def change
+    create_table :troops do |t|
+      t.string :name
+      t.string :number
+      t.string :type
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :meetup_location
+      t.text :about_us
+      
+      t.timestamps
+    end
+  end
+end
