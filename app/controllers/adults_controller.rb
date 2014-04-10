e0f00cbd908e7b71488d91d8b12a46071cd9347d
abmcdubb <abmcwilliams@gmail.com>
2014-04-10 16:27:37 -0400

    made sure that certain controller actions require login

--- app/controllers/adults_controller.rb
+++ app/controllers/adults_controller.rb
@@ -3,15 +3,17 @@ class AdultsController < ApplicationController
 
 # before_filter :authenticate_user!, except: [:index, :show]
 
-  before_filter do
+  before_filter :login_required, :except => [:show,:index] 
+
+  before_action :set_adult, only: [:show, :edit, :update, :destroy]
+
+  def login_required
     if current_adult != nil
-     authenticate_adult!        
+     authenticate_adult!    
     else
      authenticate_scout!
     end
   end
-
-  before_action :set_adult, only: [:show, :edit, :update, :destroy]
 
   def index
     @adults = Adult.all
