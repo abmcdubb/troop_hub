167010456c16ff851bec1eba8eb70f85902db233
abmcdubb <abmcwilliams@gmail.com>
2014-04-10 06:42:22 -0400

    updated controller and form

--- app/helpers/adults_helper.rb
+++ app/helpers/adults_helper.rb
@@ -1,2 +1,9 @@
 module AdultsHelper
+
+  def add_skills_to_adults(adult_id, skill_ids, skill_descriptions)
+    skill_ids.each do |skill_id|
+      AdultSkill.create(skill_id: skill_id, adult_id: adult_id, skill_description: skill_descriptions[skill_id.to_i - 1])
+    end
+  end
+
 end
--- app/models/adult.rb
+++ app/models/adult.rb
@@ -2,4 +2,7 @@ class Adult < ActiveRecord::Base
   belongs_to :troop
   has_many :adult_skills
   has_many :skills, through: :adult_skills
+
+
+
 end
