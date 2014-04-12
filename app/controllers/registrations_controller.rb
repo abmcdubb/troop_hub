75e039808e92e11e6951c6b3ceb09567b64f891f
abmcdubb <abmcwilliams@gmail.com>
2014-04-12 15:00:25 -0400

    finished styling registration page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,28 @@
 class RegistrationsController < Devise::RegistrationsController
   # before_filter :configure_permitted_parameters, :only => [:create]
+  include AdultsHelper
+  before_filter :configure_permitted_parameters, :only => [:create]
   
   def new
     @skills = Skill.all
     super
   end
 
-  before_filter :configure_permitted_parameters, :only => [:create]
+  def create  
+    @skills = Skill.all
+    super
+    add_skills_to_adults(resource.id, skills_params[:skill_ids], skills_params[:descriptions])
+  end
+
+
 
   protected
 
   def configure_permitted_parameters
-    binding.pry
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:adult_role, :name, :phone_number, :email, :password, :password_confirmation) }
+  end
+
+  def skills_params
+    params.require(:adult).permit(:skill_ids => [], :descriptions => [])
   end
 end
--- app/helpers/adults_helper.rb
+++ app/helpers/adults_helper.rb
@@ -6,4 +6,34 @@ module AdultsHelper
     end
   end
 
+  def placeholder(skill_name)
+    case skill_name
+    when "Music"
+      "Viola, Piano, Flute"
+    when "Health & Safety"
+      "CPR, Bike Safety"
+    when "Cooking"
+      "i.e.Baking, Stir Fry"
+    when "Outdoor Skills"
+      "Camping, Fishing"
+    when "Nature, Ecology"
+      "Bird Watching, Recycling"
+    when "Photography"
+      "Black & White, Still Life, SLR"
+    when "Arts & Crafts"
+      "Origami, Painting"
+    when "Math"
+      "Division, Algebra, Statistics"
+    when "Computers"
+      "Ruby on Rails, HTML"
+    when "Folk Dance & Games"
+      "Salsa, Chess"
+    when "Sewing, Needle Arts"
+      "Knitting, Crochet"
+    when "Other"
+      "Parkour, Marketing"
+    end
+
+  end
+
 end
