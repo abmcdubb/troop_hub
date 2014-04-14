4ada6bbb5996ee69c8853aff6161e578c080ce36
Msea <Msea0j@gmail.com>
2014-04-14 13:53:37 -0400

    joined scouts and badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,7 @@
 class Badge < ActiveRecord::Base
     has_many :event_badges
     has_many :events, through: :event_badges
+    has_many :scout_badges
     has_many :scouts, through: :scout_badges
 end
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,4 +7,16 @@ class Photo < ActiveRecord::Base
         @photo.photoable_type = @photo.photoable_type.camelize
         @photo.save
     end
+
+    def on_page=(string)
+        if string == "true"
+            Photo.all.where(photoable_type: self.photoable_type).where(photoable_id: self.photoable_id).each do |photo|
+                photo.on_page = "false"
+                photo.save
+            end
+            super
+        else
+            super
+        end
+    end #doesn't account for if shovel
 end
