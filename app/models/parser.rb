5572b80cac8cbfb564e6d66c8696b48bf8dc1cfe
emmaife <emma.s.ife@gmail.com>
2014-04-15 16:29:52 -0400

    badge scraping

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,26 +1,18 @@
 class Parser
 
-html = File.read('http://forgirls.girlscouts.org/home/badgeexplorer/')
+  html = File.read('./gs_site.html')
 
-badge_list = Nokogiri::HTML(html)
+  badge_list = Nokogiri::HTML(html)
 
-badges = {}
+  badge_list.css("li").each do |b|
+    if b.attr('thetitle') != nil
+      badge = Badge.new
+  		badge.logo = b.css('img').attr('src').text
+  		badge.name = b.attr("thetitle")
+  		badge.troop_type = b.attr("agelevel")
+  		badge.category = b.attr("subcategory")
+      badge.save
+    end
+  end
 
-#replace css selectors:
-
-
-
-badge_list.css("div#badge-holder-container").each do |project|
-	title = badge.css("h2.bbcard_name strong a").text.to_sym
-	puts badges[title] = {
-		:logo => badges.css('li').attr('img').attr('src').text,
-		:name => badge.css("p.bbcard_blurb").text,
-		:troop_type => badge.css("ul.badge-meta span.location-name").text,
-		:category => badge.css("ul.badge-stats li.first.funded strong").text.gsub("%","").to_i
-		:description => badge.css("ewkrughewkrghekwhg").text.ergkuhewkgh
-	}
 end
-
-# badges.css('li').attr('agelevel')
-# badges.css('li').attr('img').attr('src').text
-# badges.css('li').attr('agelevel')
