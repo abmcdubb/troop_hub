31ba034093be227a2a7cb4388b9e6503fb7c656d
emmaife <emma.s.ife@gmail.com>
2014-04-15 16:32:46 -0400

    adjusted scouts_badges db

--- db/migrate/20140411204835_create_scouts_badges.rb
+++ db/migrate/20140411204835_create_scouts_badges.rb
@@ -1,8 +1,7 @@
 class CreateScoutsBadges < ActiveRecord::Migration
   def change
     create_table :scouts_badges do |t|
-      t.index :scout_id
-      t.index :badge_id
+  
 
       t.timestamps
     end
