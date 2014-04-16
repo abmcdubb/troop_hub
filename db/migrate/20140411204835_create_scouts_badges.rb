cc222ebc8441aabbdc7e2ae9ac6937a1547f1ff9
abmcdubb <abmcwilliams@gmail.com>
2014-04-16 13:38:17 -0400

    added new better profile pictures

--- db/migrate/20140411204835_create_scouts_badges.rb
+++ db/migrate/20140411204835_create_scouts_badges.rb
@@ -1,9 +1,6 @@
 class CreateScoutsBadges < ActiveRecord::Migration
   def change
     create_table :scouts_badges do |t|
-      t.index :scout_id
-      t.index :badge_id
-
       t.timestamps
     end
   end
