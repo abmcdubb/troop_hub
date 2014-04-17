1a7aa9ccc37909d46a9841d05f5a4314dc3eedb9
emmaife <emma.s.ife@gmail.com>
2014-04-17 17:09:42 -0400

    changed links (troop_users). created user profile link

--- config/routes.rb
+++ config/routes.rb
@@ -9,23 +9,43 @@ Rails.application.routes.draw do
   post 'newsletters' => 'newsletters#create'
   get 'newsletters/:id' => 'newsletters#show'
   get 'newsletters' => 'newsletters#index'
- 
+
+
+
+#-------Troop-Specific Routes----------
+
   get '/troops' => 'troops#index'
   get 'troops/new' => 'troops#new'
   post '/troops' => 'troops#create'
-  #get 'troops/edit/:id' => 'troops#edit'
+  get 'troops/edit/:id' => 'troops#edit'
+  # get 'troops/:id' => 'troops#show', as: 'troop'
   get 'troops/:id' => 'troops#show', :as => :troop_show
+
+  patch 'troops/update/:id' => 'troops#update'
+  delete 'troops/destroy' => 'troops#destroy'
+  get '/troops/:troop_id/members' => 'users#index', :as => :troop_user
+
+  # get '/troops' => 'troops#index'
+  # get 'troops/new' => 'troops#new'
+  # post '/troops' => 'troops#create'
+
+  get '/troops/:troop_id/users/new' => 'users#new'
+  post '/troops/:troop_id/users' => 'users#create'
+  # get '/troops/:troop_id/users/:id' => 'users#show', 
+
+  #get 'troops/edit/:id' => 'troops#edit'
   #patch 'troops/update/:id' => 'troops#update'
   delete 'troops/destroy' => 'troops#destroy'
 
-  get '/troops/:troop_id/users' => 'users#index', :as => :troop_user
-  get '/troops/:troop_id/users/new' => 'users#new', :as => :new_troop_user
-  post '/troops/:troop_id/users' => 'users#create'
-  get '/troops/:troop_id/users/:id' => 'users#show', :as => :user_show
-  get '/troops/:troop_id/users/edit/:id' => 'users#edit'
-  patch '/troops/:troop_id/users/update/:id' => 'users#update'
-  delete '/troops/:troop_id/users/destroy' => 'users#destroy'
+#---Routes for Individual Users -----
+  get '/users/:user_id' => 'troop_users#show', :as => :user_show
+  get '/users/edit/:user_id' => 'troop_users#edit'
+  patch '/users/update/:user_id' => 'troop_users#update'
+  delete '/users/destroy' => 'troop_users#destroy'
 
+
+
+#-------Events---------------
   get 'events' => 'events#index', :as => :events_index
   get 'events/new'
   get 'events/search' => 'events#search_results'
@@ -36,13 +56,9 @@ Rails.application.routes.draw do
   delete 'events/:id/delete' => 'events#delete'
   get 'events/advanced_search' => 'events#advanced_search'
 
-  get '/troops' => 'troops#index'
-  get 'troops/new' => 'troops#new'
-  post '/troops' => 'troops#create'
-  get 'troops/edit/:id' => 'troops#edit'
-  get 'troops/:id' => 'troops#show', as: 'troop'
-  patch 'troops/update/:id' => 'troops#update'
-  delete 'troops/destroy' => 'troops#destroy'
+
+
+
 
   get 'troop_events/new' => 'troop_events#new'
   get 'troop_events/:troop_id/new_event' => 'troop_events#new_event', as: 'new_event'
