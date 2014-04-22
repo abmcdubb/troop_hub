fa2fcd253b81702d9d35b1567afcc08dd1969cbe
emmaife <emma.s.ife@gmail.com>
2014-04-22 19:26:46 -0400

    got rid of binding

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -10,11 +10,10 @@ autocomplete :badge, :name
   end
 
   def category_index   
-  binding.pry 
     @event = Event.new
     @skills = Skill.order(name: :asc)
     @age_levels = AgeLevel.all
     @badges = Badge.all
-    @events = Event.find_by_skill_category(params[:id].upcase).order(name: :asc).paginate(page: params[:page], per_page: 10)
+    @events = Event.find_by_skill_category(params[:id]).order(name: :asc).paginate(page: params[:page], per_page: 10)
   end
 end
