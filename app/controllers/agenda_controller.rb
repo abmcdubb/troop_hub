34e62bcdb62e81b1c66703fbafe689a9d407016a
Stephanie Chandra <stephaniechandra@gmail.com>
2014-04-22 17:35:10 -0400

    pdf generator works, with css styling

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -22,6 +22,38 @@ class AgendaController < ApplicationController
 
   def show
     @agenda = Agenda.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        # THIS SHOULD ALL BE IN A CLASS, LIKE AN UPLOADER OR SOMETHING
+        html = render_to_string(partial: 'agenda.html.erb')
+        pdf = PDFKit.new(html, :page_size => 'Letter', :quiet => false)
+        stylesheet = TroopHub::Application.assets.find_asset('application.css').source
+        filename = "agenda_#{@agenda.id}.pdf"
+        if Rails.env == 'production'
+          path = "#{RAILS_ROOT}/tmp/#{filename}"
+          stylesheet_path = "#{RAILS_ROOT}/tmp/#{filename}_css.css"
+          File.open(stylesheet_path, "w+") do |f|
+            f.write(stylesheet)
+          end
+        else
+          path = "tmp/#{filename}"
+          stylesheet_path = "tmp/#{filename}_css.css"
+          File.open(stylesheet_path, "w+") do |f|
+            f.write(stylesheet)
+          end
+        end
+        pdf.stylesheets << stylesheet_path
+        file = pdf.to_file(path)
+        AWS::S3::S3Object.store(
+          filename,
+          open(path),
+          "troop-hub",
+          access: :public_read
+        )
+        redirect_to "https://troop-hub.s3.amazonaws.com/#{filename}"
+      end
+    end
   end
 
   def edit
