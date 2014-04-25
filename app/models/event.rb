3c67d61bb2fff9ac2b1af6ef39dc4c0487e90b30
Msea <Msea0j@gmail.com>
2014-04-25 12:47:59 -0400

    fixed capatilaztion error which was screwing search by general skill categories

--- app/models/event.rb
+++ app/models/event.rb
@@ -92,14 +92,14 @@ class Event < ActiveRecord::Base
   def self.skills_for_search(skill_id)
     if skill_id.to_i > 0
       skills = Skill.where(id: skill_id)
-    elsif skill_id == "GENERAL"
+    elsif skill_id == "General"
       skills = Skill.where(category: 'GENERAL')
     elsif skill_id == "STEM"
       skills = Skill.where(category: 'STEM')
-    elsif skill_id == "BUSINESS SMARTS"
+    elsif skill_id == 'Business Smarts'
       skills = Skill.where(category: 'BUSINESS SMARTS')
-    elsif skill_id == "NATURE & ECOLOGY"
-      skills = Skill.where(category: 'NATURE & ECOLOGY')
+    elsif skill_id == "Nature and Ecology"
+      skills = Skill.where(category: 'NATURE AND ECOLOGY')
     else
       skills = Skill.all
     end
@@ -161,7 +161,6 @@ class Event < ActiveRecord::Base
       else
         badge_string += " or #{b} badges"
       end
-      # if length two no comma?
     end
     " badges"
     badge_string
